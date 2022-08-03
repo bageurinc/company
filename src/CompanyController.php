00797abf7e68a0d53e94356def32a41e4e2ee838
naufal <naufalrandialdiansyah@gmail.com>
2022-08-03 14:36:09 +0700

    ..

--- src/CompanyController.php
+++ src/CompanyController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Bageur\Company\Model\company;
 use Bageur\Company\Processors\Helper;
+use Illuminate\Support\Facades\Http;
 use Validator;
 class CompanyController extends Controller
 {
